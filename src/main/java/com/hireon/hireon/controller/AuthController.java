27b5043cfb4bcd301473830cbc3c44d9646ac199
S-nusrath <nusrath4786@gmail.com>
2026-02-21 23:17:49 +0530

    Updated

--- src/main/java/com/hireon/hireon/controller/AuthController.java
+++ src/main/java/com/hireon/hireon/controller/AuthController.java
@@ -1,19 +1,39 @@
+// package com.hireon.hireon.controller;
+
+// import com.hireon.hireon.dto.LoginRequest;
+// import org.springframework.web.bind.annotation.*;
+
+// @RestController
+// @RequestMapping("/api/auth")
+// public class AuthController {
+
+//     @PostMapping("/login")
+//     public String login(@RequestBody LoginRequest request) {
+
+//         System.out.println(request.getEmail());
+//         System.out.println(request.getPassword());
+//          System.out.println("Role: " + request.getRole());
+
+//         return "Login Success";
+//     }
+// }
+
 package com.hireon.hireon.controller;
 
 import com.hireon.hireon.dto.LoginRequest;
+import com.hireon.hireon.service.AuthService;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
 @RestController
 @RequestMapping("/api/auth")
 public class AuthController {
 
+    @Autowired
+    private AuthService authService;
+
     @PostMapping("/login")
     public String login(@RequestBody LoginRequest request) {
-
-        System.out.println(request.getEmail());
-        System.out.println(request.getPassword());
-         System.out.println("Role: " + request.getRole());
-
-        return "Login Success";
+        return authService.login(request);
     }
 }
