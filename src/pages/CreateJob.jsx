63f01ac93ab64e844d178d4deedbcb4473f7c0ca
shaiksurayya <shaiksurayyatabassum@gmail.com>
2026-03-08 10:43:38 +0530

    storing jobs in db created by admin

--- src/pages/CreateJob.jsx
+++ src/pages/CreateJob.jsx
@@ -1,4 +1,85 @@
+// import { useState } from "react";
+
+// export default function CreateJob(){
+
+// const [title,setTitle] = useState("");
+// const [company,setCompany] = useState("");
+// const [skills,setSkills] = useState("");
+// const [experience,setExperience] = useState("");
+
+// const handleSubmit=(e)=>{
+
+// e.preventDefault();
+
+// console.log({
+// title,
+// company,
+// skills:skills.split(","),
+// experience
+// });
+
+// alert("Job Created!");
+
+// }
+
+// return(
+
+// <div className="p-6">
+
+// <h1 className="text-xl font-bold mb-4">
+// Create Job
+// </h1>
+
+// <form onSubmit={handleSubmit} className="space-y-3">
+
+// <input
+// placeholder="Job Title"
+// value={title}
+// onChange={(e)=>setTitle(e.target.value)}
+// className="border p-2 w-full"
+// />
+
+// <input
+// placeholder="Company"
+// value={company}
+// onChange={(e)=>setCompany(e.target.value)}
+// className="border p-2 w-full"
+// />
+
+// <input
+// placeholder="Required Skills (React,Node)"
+// value={skills}
+// onChange={(e)=>setSkills(e.target.value)}
+// className="border p-2 w-full"
+// />
+
+// <select
+// value={experience}
+// onChange={(e)=>setExperience(e.target.value)}
+// className="border p-2 w-full"
+// >
+
+// <option value="">Select Experience</option>
+// <option value="0-1 years">0-1 Years</option>
+// <option value="1-3 years">1-3 Years</option>
+// <option value="3-5 years">3-5 Years</option>
+
+// </select>
+
+// <button className="bg-blue-500 text-white px-4 py-2">
+// Create Job
+// </button>
+
+// </form>
+
+// </div>
+
+// )
+
+// }
+
 import { useState } from "react";
+import axios from "axios";
 
 export default function CreateJob(){
 
@@ -7,20 +88,42 @@ const [company,setCompany] = useState("");
 const [skills,setSkills] = useState("");
 const [experience,setExperience] = useState("");
 
-const handleSubmit=(e)=>{
+const handleSubmit = async (e) => {
 
 e.preventDefault();
 
-console.log({
-title,
-company,
-skills:skills.split(","),
-experience
-});
+const jobData = {
+jobTitle: title,
+company: company,
+requiredSkills: skills,
+experience: experience
+};
 
-alert("Job Created!");
+try{
+
+const res = await axios.post(
+"http://localhost:8080/api/jobs",
+jobData
+);
+
+console.log(res.data);
+
+alert("Job Created Successfully ✅");
+
+// reset form
+setTitle("");
+setCompany("");
+setSkills("");
+setExperience("");
+
+}catch(error){
+
+console.error(error);
+alert("Error creating job");
 
 }
+
+};
 
 return(
 
