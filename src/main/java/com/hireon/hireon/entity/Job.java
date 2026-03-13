a481fb72cfb457de84811536befdca6cf5ce5788
shaiksurayya <shaiksurayyatabassum@gmail.com>
2026-03-13 19:48:29 +0530

    backend for creating job

--- src/main/java/com/hireon/hireon/entity/Job.java
+++ src/main/java/com/hireon/hireon/entity/Job.java
@@ -13,11 +13,70 @@ public class Job {
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
-    private String jobTitle;
+    private String title;
 
     private String company;
 
-    private String requiredSkills;
+    private String location;
+
+    private String jobType;
 
     private String experience;
+
+    private String salaryMin;
+
+    private String salaryMax;
+
+    private String skills;
+
+    @Column(length = 2000)
+    private String description;
+
+    private Integer openings;
+
+    private String deadline;
+
 }
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+// package com.hireon.hireon.entity;
+
+// import jakarta.persistence.*;
+// import lombok.*;
+
+// @Entity
+// @Data
+// @NoArgsConstructor
+// @AllArgsConstructor
+// public class Job {
+
+//     @Id
+//     @GeneratedValue(strategy = GenerationType.IDENTITY)
+//     private Long id;
+
+//     private String jobTitle;
+
+//     private String company;
+
+//     private String requiredSkills;
+
+//     private String experience;
+// }
